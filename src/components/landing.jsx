5aed98fcff7be4b7c8d90ad0a89e0d5fb594aa95
sourabh <sourabhp0295@gmail.com>
2023-08-17 11:28:34 +0530

    signup rest form

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -12,10 +12,17 @@ import Dashboard from './maestro/dashboard'
 
 import Login from './login'
 import Signup from './signup/signup'
+import Otp from './signup/otp'
+import Aftersignup1 from './signup/aftersignup1';
+import Aftersignup2 from './signup/aftersignup2'; 
+import Aftersignup3 from './signup/aftersignup3';
 import NotFound from './notfound'
 import Navbar from './navbar'
 import Footer from './footer'
 import Test from './test'
+
+
+
 
 function Landing() {
 
@@ -35,6 +42,10 @@ function Landing() {
 
           <Route exact path="/login" component={Login}/>
           <Route exact path="/signup" component={Signup}/>
+          <Route exact path="/signup/otp" component={Otp}/>
+          <Route exact path="/signup/aftersignup1" component={Aftersignup1}/>
+          <Route exact path="/signup/aftersignup2" component={Aftersignup2}/>
+          <Route exact path="/signup/aftersignup3" component={Aftersignup3}/>
           <Route exact path="/test" component={Test}/>
           <Route path="/maestro/dashboard/"><Dashboard/></Route>
           <Route path="*" component={NotFound}/>
